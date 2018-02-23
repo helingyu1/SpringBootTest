566d6a1c0840e19df7ab74ec4267e65d4ec737f3
YuQing <helingyu1122@qq.com>
2018-02-23 22:12:13 +0800

    删除无效引用

--- src/main/java/com/hly/config/RedisCacheConfiguration.java
+++ src/main/java/com/hly/config/RedisCacheConfiguration.java
@@ -1,6 +1,5 @@
 package com.hly.config;
 
-import com.sun.istack.internal.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
